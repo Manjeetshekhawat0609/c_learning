e6151fac477b25e0314debd5cd6b54ff9c541114
Manjeet Singh Shekhawat <manjeetshekhawat0609@gmail.com>
2026-07-31 18:53:59 +0530

    update all file

--- extra-questions.c
+++ extra-questions.c
@@ -24,8 +24,10 @@
 int main(){
     int a = 10;
     int b = 20;
+    printf("Before swapping: a = %d, b = %d\n", a, b);
     a = a + b;
     b = a - b;
     a = a - b;
     printf("After swapping: a = %d, b = %d\n", a, b);
+    return 0;
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
-// int main() {
-//     int side , area;
-//     printf("Enter the side of the square: ");
-//     scanf("%d", &side);
-//     area = side * side;
-//     printf(" the area of the square is %d\n", area);
-//     return 0;
-// }
+int main() {
+    int side , area;
+    printf("Enter the side of the square: ");
+    scanf("%d", &side);
+    area = side * side;
+    printf(" the area of the square is %d\n", area);
+    return 0;
+}
