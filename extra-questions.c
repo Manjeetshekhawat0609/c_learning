8f9e2103c5dbd04b704389c74609aabbdee89c5b
Manjeet Singh Shekhawat <manjeetshekhawat0609@gmail.com>
2026-07-31 18:52:46 +0530

    update all file and add this new file

---
+++ extra-questions.c
@@ -0,0 +1,31 @@
+// #include <stdio.h>
+// int main() {
+//     int side , area;
+//     printf("Enter the side of the square: ");
+//     scanf("%d", &side);
+//     area = side * side;
+//     printf(" the area of the square is %d\n", area);
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main(){
+//     int side, volume;
+//     printf("Enter the side of the cube: ");
+//     scanf("%d", &side);
+//     volume = side * side * side;
+//     printf(" the volume of the cube is %d\n", volume);
+//     return 0;
+// }
+
+
+#include<stdio.h>
+int main(){
+    int a = 10;
+    int b = 20;
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("After swapping: a = %d, b = %d\n", a, b);
+}
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+// int main() {
+//     int side , area;
+//     printf("Enter the side of the square: ");
+//     scanf("%d", &side);
+//     area = side * side;
+//     printf(" the area of the square is %d\n", area);
+//     return 0;
+// }
