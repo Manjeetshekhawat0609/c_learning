da76b78d815a1152f7ac060c36e180f1e0a2404d
Manjeet Singh Shekhawat <manjeetshekhawat0609@gmail.com>
2026-08-13 21:59:09 +0530

    add new codes

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,12 +1,21 @@
-#include<stdio.h>
-int main(){
-    int year;
-    printf("Enter a year: ");
-    scanf("%d", &year);
-    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-        printf("%d is a leap year.\n", year);
-    } else {
-        printf("%d is not a leap year.\n", year);
-    }
+
+//     printf("the volume of cylinder is %f", 3.14*r*r*h);
+//     return 0;
+// }
+
+
+#include <stdio.h>
+int main()
+{
+    int c,f;
+    printf("enter the temperature in celsius\n");
+    scanf("%d", &c);
+    f = (c * 9/5) + 32;
+    printf("the temperature in fahrenheit is %d", f);
     return 0;
 }
+
+
+
+
+
